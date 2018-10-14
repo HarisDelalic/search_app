17370ea001fea6b29debff1ff84af6cc1dfcb811
delalicharis@gmail.com <delalicharis@gmail.com>
2018-10-14 20:35:57 +0200

    implement search#index funtionality

---
+++ spec/factories/searches.rb
@@ -0,0 +1,6 @@
+FactoryBot.define do
+  factory :search do
+    ip '170.170.170.170'
+    query 'How to'
+  end
+end
--- spec/features/searches_spec.rb
+++ spec/features/searches_spec.rb
@@ -1,10 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe 'Searches managment' do
-  let(:search) { create(:search, query: 'How to', ip: '170.170.170.170') }
+  let!(:search) { create(:search) }
   it 'User lists search items' do
     visit searches_path
-    expect(page).to have_content('How to')
-    expect(page).to have_content('170.170.170.170')
+    expect(page).to have_content(search.query)
+    expect(page).to have_content(search.ip)
   end
 end
