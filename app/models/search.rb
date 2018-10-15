04aad49abf4d9ddc8bf70d3458d9b7500ca3a987
delalicharis@gmail.com <delalicharis@gmail.com>
2018-10-15 15:17:26 +0200

    small code cleanup, layout improvements

--- app/models/search.rb
+++ app/models/search.rb
@@ -5,6 +5,7 @@ class Search < ApplicationRecord
   scope :by_ip, ->(ip) { where(ip: ip) }
 
   def contained_in_existing_search?
+    return false if without_ip
     existing_ip_queries = Search.by_ip(ip)
     existing_ip_queries.each do |existing_ip_query|
       return true if existing_ip_query.query.include?(query)
@@ -13,13 +14,19 @@ class Search < ApplicationRecord
   end
 
   def remove_contained_searches
+    return false if without_ip
     existing_ip_searches = Search.by_ip(ip)
     remove_contains(existing_ip_searches)
   end
 
+  private
   def remove_contains(existing_ip_searches)
     existing_ip_searches.each do |existing_ip_query|
       existing_ip_query.destroy if query.include?(existing_ip_query.query)
     end
+  end
+
+  def without_ip
+    ip.nil?
   end
 end
--- app/services/searcher.rb
+++ app/services/searcher.rb
@@ -6,12 +6,20 @@ class Searcher
     @search_params = search_params
   end
 
+  # if new search is substring of existing search, like user searches "change"
+  # but there is already search from same ip to string "change money", in that case
+  # nothing is done in database.
+  # Other case: user searches "change money", but in database we have entry "change",
+  # again from same IP, in that case search with query "change" should be deleted, and
+  # search with query "change money persisted"
   def run
     Search.transaction do
       search.assign_attributes(search_params)
       search.ip = ip
-      search.remove_contained_searches
-      search.save unless search.contained_in_existing_search?
+      unless search.contained_in_existing_search?
+        search.remove_contained_searches
+        search.save
+      end
     end
   end
 end
